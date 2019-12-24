98ddfcfc30a692c0d198f1ba1c1c4514066db407
Thomas Argent <tfargent@gmail.com>
2019-12-23 22:44:40 -0500

    tests

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const axios = require("axios");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
+
+// prompt questions 
 function promptUser() {
     inquirer.prompt([
         {
@@ -17,16 +19,20 @@ function promptUser() {
             message: "Choose a color",
             name: "color",
             choices: [
-                "Red",
-                "Green",
-                "Blue"
+                "red",
+                "green",
+                "blue",
+                "pink"
             ]
         }
-        
-    ]).then(function (response) {
-        const queryUrl = `https://api.github.com/users/${response.username}`;
-       const color = response.color
+
+        // axios to gather data from github
+    ]).then(function ({ username, color }) {
+        const queryUrl = `https://api.github.com/users/${username}`;
+
         axios.get(queryUrl).then(function (res) {
+            console.log(res.data)
+            console.log(color)
             const html = generateHTML(res, color);
             console.log(res.data)
 
@@ -42,31 +48,258 @@ function promptUser() {
 
 }
 
+// function generateHTML(res, color) {
+//     return `
+//   <!DOCTYPE html>
+//   <html lang="en">
+//   <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//     <title>Document</title>
+//   </head>
+//   <body>
+//   <div>
+//     <h1 style="color:${color}">${res.data.name}</h1>
+//     <img id="bio" src="${res.data.avatar_url}" alt="${res.data.name}">
+//     <p>${res.data.bio}</p>
+//     <p>${res.data.company}</p>
+//     <p>Repo URL: <a href= ${res.data.html_url}>${res.data.name}'s Repo URL</a></p>
+//     <p>Public Repos: ${res.data.public_repos}</p>
+//     <p>Followers: ${res.data.followers}</p>
+//     <p>Following: ${res.data.following}</p>
+//     <p>Location: ${res.data.location}</p>
+//   </div>
+//   </body>
+//   </html>`;
+// }
+
+
+
+
+
+
+
+
+
+const colors = {
+    green: {
+        wrapperBackground: "#E6E1C3",
+        headerBackground: "#C1C72C",
+        headerColor: "black",
+        photoBorderColor: "#black"
+    },
+    blue: {
+        wrapperBackground: "#5F64D3",
+        headerBackground: "#26175A",
+        headerColor: "white",
+        photoBorderColor: "#73448C"
+    },
+    pink: {
+        wrapperBackground: "#879CDF",
+        headerBackground: "#FF8374",
+        headerColor: "white",
+        photoBorderColor: "#FEE24C"
+    },
+    red: {
+        wrapperBackground: "#DE9967",
+        headerBackground: "#870603",
+        headerColor: "white",
+        photoBorderColor: "white"
+    }
+};
+
+// generate HTML
 function generateHTML(res, color) {
-    return `
-  <!DOCTYPE html>
+    return `<!DOCTYPE html>
   <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Document</title>
-  </head>
-  <body>
-  <div>
-    <h1 style="color:${color}">${res.data.name}</h1>
+     <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+        <title>Document</title>
+        <style>
+            @page {
+              margin: 0;
+            }
+           *,
+           *::after,
+           *::before {
+           box-sizing: border-box;
+           }
+           html, body {
+           padding: 0;
+           margin: 0;
+           }
+           html, body, .wrapper {
+           height: 100%;
+           }
+           .wrapper {
+           background-color: ${colors[color].wrapperBackground};
+           padding-top: 100px;
+           }
+           body {
+           background-color: white;
+           -webkit-print-color-adjust: exact !important;
+           font-family: 'Cabin', sans-serif;
+           }
+           main {
+           background-color: #E9EDEE;
+           height: auto;
+           padding-top: 30px;
+           }
+           h1, h2, h3, h4, h5, h6 {
+           font-family: 'BioRhyme', serif;
+           margin: 0;
+           }
+           h1 {
+           font-size: 3em;
+           }
+           h2 {
+           font-size: 2.5em;
+           }
+           h3 {
+           font-size: 2em;
+           }
+           h4 {
+           font-size: 1.5em;
+           }
+           h5 {
+           font-size: 1.3em;
+           }
+           h6 {
+           font-size: 1.2em;
+           }
+           .photo-header {
+           position: relative;
+           margin: 0 auto;
+           margin-bottom: -50px;
+           display: flex;
+           justify-content: center;
+           flex-wrap: wrap;
+           background-color: ${colors[color].headerBackground};
+           color: ${colors[color].headerColor};
+           padding: 10px;
+           width: 95%;
+           border-radius: 6px;
+           }
+           .photo-header img {
+           width: 250px;
+           height: 250px;
+           border-radius: 50%;
+           object-fit: cover;
+           margin-top: -75px;
+           border: 6px solid ${colors[color].photoBorderColor};
+           box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
+           }
+           .photo-header h1, .photo-header h2 {
+           width: 100%;
+           text-align: center;
+           }
+           .photo-header h1 {
+           margin-top: 10px;
+           }
+           .links-nav {
+           width: 100%;
+           text-align: center;
+           padding: 20px 0;
+           font-size: 1.1em;
+           }
+           .nav-link {
+           display: inline-block;
+           margin: 5px 10px;
+           }
+           .workExp-date {
+           font-style: italic;
+           font-size: .7em;
+           text-align: right;
+           margin-top: 10px;
+           }
+           .container {
+           padding: 50px;
+           padding-left: 100px;
+           padding-right: 100px;
+           }
+  
+           .row {
+             display: flex;
+             flex-wrap: wrap;
+             justify-content: space-between;
+             margin-top: 20px;
+             margin-bottom: 20px;
+           }
+  
+           .card {
+             padding: 20px;
+             border-radius: 6px;
+             background-color: ${colors[color].headerBackground};
+             color: ${colors[color].headerColor};
+             margin: 20px;
+           }
+           
+           .col {
+           flex: 1;
+           text-align: center;
+           }
+  
+           a, a:hover {
+           text-decoration: none;
+           color: inherit;
+           font-weight: bold;
+           }
+  
+           @media print { 
+            body { 
+              zoom: .75; 
+            } 
+           }
+        </style>
+        
+        <body>
+    <div class = "wrapper">
+    <div class = "container photo-header">
     <img id="bio" src="${res.data.avatar_url}" alt="${res.data.name}">
-    <p>${res.data.bio}</p>
-    <p>${res.data.company}</p>
-    <p>Repo URL: <a href= ${res.data.html_url}>${res.data.name}'s Repo URL</a></p>
-    <p>Public Repos: ${res.data.public_repos}</p>
-    <p>Followers: ${res.data.followers}</p>
-    <p>Following: ${res.data.following}</p>
-    <p>Location: ${res.data.location}</p>
-  </div>
+    <h1>Hi!</h1>
+    <h1>My name is ${res.data.name}</h1>
+    <h3>I work at ${res.data.company}</h3>
+    <div class="links-nav">
+          <a class="nav-link" href="https://www.google.com/maps/d/viewer?hl=en&ie=UTF8&msa=0&ll=35.173633959695465%2C-80.63937599999997&spn=0.387581%2C0.559616&source=embed&mid=1LyUjd4I7rYUxc8EcFmy_VgRqxMA&z=10"><i class="fas fa-location-arrow fa-1x"
+          aria-hidden="true"></i> ${res.data.location}</a>
+          <a class="nav-link" href="${res.data.html_url}  "><i class="fab fa-github-square fa-1x"
+          aria-hidden="true"></i> GitHub</a>
+          <a class="nav-link" href="${res.data.blog}  "><i class="fas fa-rss-square fa-1x"
+          aria-hidden="true"></i> Blog</a>
+          </div>
+          </div>
+    <main>
+    <div class = "container">
+    <h5>${res.data.bio}</h5>
+    <div class="row">
+           <div class = "col">
+                <div class ="card"><h3>Public Repos: </h3> <h3>${res.data.public_repos}</h3></div>
+                </div>
+                <div class = "col">
+                <div class ="card"><h3>Followers: </h3> <h3>${res.data.followers}</h3></div>
+                </div>
+                </div>
+                <div class = "row">
+                <div class = "col">
+                <div class ="card"><h3>Github Stars: </h3> <h3>${res.data.following}</h3></div>
+                </div>
+                <div class = "col">
+                <div class ="card"><h3>Following: </h3> <h3>${res.data.following}</h3></div>
+                </div>
+                </div>
+                </main>
+                </div> 
+    
+    
+ 
   </body>
-  </html>`;
+  </html>`
 }
 
-promptUser()
 
+promptUser()
